d5dbbdf46bdc9b236062b4b2b1aa3054338ed88e
maunonen <santari33@gmail.com>
2020-07-20 19:02:38 +0300

    Profile fields requiered=False, profile template breadcrumbs style text-muted

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,28 +4,26 @@ from django.utils.translation import gettext as _
 from django.contrib.auth.models import User
 
 class ProfileForm(forms.Form): 
-    """ username = forms.CharField(required=True, max_length=50, widget=forms.TextInput(
-        attrs={'class' : 'form-control form-control-lg', 'placeholder' : 'John'}
-    )) """
+    
     email = forms.EmailField(required=True, widget=forms.EmailInput(
         attrs={'class': 'form-control form-control', 'placeholder' : 'email@example.com'}
     ))
-    first_name = forms.CharField(required=True, max_length=50, widget=forms.TextInput(
+    first_name = forms.CharField(required=False, max_length=50, widget=forms.TextInput(
         attrs={'class' : 'form-control', 'placeholder' : 'John', 'aria-describedby' : "firstNamePrepend"}
     ))
-    last_name = forms.CharField(required=True, max_length=50, widget=forms.TextInput(
+    last_name = forms.CharField(required=False, max_length=50, widget=forms.TextInput(
         attrs={'class' : 'form-control', 'placeholder' : 'Doe','aria-describedby' : "lastNamePrepend"}
     ))
-    phone_number = forms.CharField(required=True, max_length=20, widget=forms.TextInput(
+    phone_number = forms.CharField(required=False, max_length=20, widget=forms.TextInput(
         attrs={'class' : 'form-control', 'placeholder' : '044455555',  'aria-describedby' : "phoneNumberPrepend"}
     ))
-    town = forms.CharField(required=True, max_length=20, widget=forms.TextInput(
+    town = forms.CharField(required=False, max_length=20, widget=forms.TextInput(
         attrs={'class' : 'form-control', 'placeholder' : 'Vantaa', 'aria-describedby' : "townPrepend"}
     ))
-    postal_code = forms.CharField(required=True, max_length=20, widget=forms.TextInput(
+    postal_code = forms.CharField(required=False, max_length=20, widget=forms.TextInput(
         attrs={'class' : 'form-control', 'placeholder' : '00100', 'aria-describedby' : "postalCodePrepend"}
     ))
-    address = forms.CharField(required=True, max_length=50, widget=forms.TextInput(
+    address = forms.CharField(required=False, max_length=50, widget=forms.TextInput(
         attrs={'class' : 'form-control', 'placeholder' : 'Aleksanterinkatu 2 A 1', 'aria-describedby' : "addressPrepend"}
     ))
 
